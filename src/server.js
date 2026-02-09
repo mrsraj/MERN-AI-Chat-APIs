b331fb44f2b1b11ed69e83e55f3f10104b8f9eb3
Sachin Raj <sachinraj6392@gmail.com>
2026-02-09 23:40:15 +0530

    code

--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
 require("dotenv").config();
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
+const cors = require("cors");
 
 const passport = require("passport");
 require("./config/passport");
-
 
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 //call db connection
 connectDB();
+
+const corsOptions = {
+    origin: "http://localhost:3000", // frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+// app.use(cors(corsOptions));
+app.use(cors());
 
 // Json
 app.use(express.json());
@@ -18,12 +28,21 @@ app.use(passport.initialize());
 //router import
 const authrouter = require("./routes/auth.router");
 
+//API Call 
 app.use("/api/v1/auth", authrouter);
 
+
+//Just for testing
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Server is running...");
 });
 
+// CATCH-ALL ROUTE (must be last)
+app.use((req, res) => {
+    res.status(404).json({ message: "Path does not exist" });
+});
+
+//Server listen
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
